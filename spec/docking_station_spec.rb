64209183bf304422741a219f4be3fc6b7b5480eb
charlie-casserly <charliecasserly215@gmail.com>
2021-04-29 14:14:15 +0100

    Step 11 Complete

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,6 +5,17 @@ describe DockingStation do
 
   it "creates a bike object" do
     bike = subject.release_bike
-    expect(bike.working?).to eq true
+    expect(bike).to be_working
+  end
+
+  it "docks something" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
 end
