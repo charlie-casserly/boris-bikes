7707aed91b5f37444ae542bb2230ae882046c4f1
charlie-casserly <charliecasserly215@gmail.com>
2021-04-29 15:02:10 +0100

    Step 12 complete

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,8 +3,13 @@ require_relative 'bike'
 class DockingStation
   attr_reader :bike
 
+  def bike
+    @bike
+  end
+
   def release_bike
-    Bike.new
+    fail "No bike available" unless @bike
+    @bike
   end
 
   def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,9 +3,17 @@ require "docking_station"
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
 
-  it "creates a bike object" do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it "creates a bike object" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it "raises an error when no bike available" do
+      bike = Bike.new
+      expect {subject.release_bike}.to raise_error('No bike available')
+    end
   end
 
   it "docks something" do
@@ -19,3 +27,4 @@ describe DockingStation do
     expect(subject.bike).to eq bike
   end
 end
+
